79fdfa90e2de5526f96eb9a27afcfc79955b569b
GangumoluGeethikNagaSaiSreeRakesh <gnssrakesh@gmail.com>
2024-10-25 12:06:51 +0530

    Create given number is positive or negative.c

---
+++ given number is positive or negative.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    int a;
+    printf("enter value of a:");
+    scanf("%d",&a);
+    if(a>0)
+    {
+        printf("positive");
+    }
+    else
+    {
+        printf("negative");
+    }
+}
+    
