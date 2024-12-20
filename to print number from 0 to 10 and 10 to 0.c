5cea5ce4b405a662af9b48845de47c4ff12753db
GangumoluGeethikNagaSaiSreeRakesh <gnssrakesh@gmail.com>
2024-12-20 11:49:07 +0530

    Update to print number from 0 to 10 and 10 to 0.c

--- to print number from 0 to 10 and 10 to 0.c
+++ to print number from 0 to 10 and 10 to 0.c
@@ -4,13 +4,13 @@ void main()
     int i=0,j=10;
     while(i<=10)
     {
-        printf("%d",i);
+        printf("%d ",i);
         i++;
     }
     printf("\n");
     while(j>=0)
     {
-        printf("%d",j);
+        printf("%d ",j);
         j--;
     }
 }
